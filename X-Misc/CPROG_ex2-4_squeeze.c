a99e025aa90b4a415071f9a96cd97d658290f3b7
Peter Smith <peter@cruelandunusual.be>
2025-07-18 14:46:57 +0100

    added original code for comparison against the solution

--- X-Misc/CPROG_ex2-4_squeeze.c
+++ X-Misc/CPROG_ex2-4_squeeze.c
@@ -46,3 +46,36 @@ void squeeze(char s1[], char s2[])
     }
     s1[j] = '\0';
 }
+
+
+/*
+original code for squeeze(s, c):
+
+
+void squeeze(char s[], char c);
+
+int main(void)
+{
+    char string1[] = "hello";
+    char s = 'l';
+    printf("before squeeze()\n");
+    printf("%s\n", string1);
+    printf("%c\n", s);
+    squeeze(string1, s);
+    printf("after squeeze()\n");
+    printf("%s\n", string1);
+    printf("%c\n", s);
+}
+
+void squeeze(char s[], char c)
+{
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; ++i) {
+	if (s[i] != c) {
+	    s[j++] = s[i];
+	}
+    }
+    s[j] = '\0';
+}
+
+*/

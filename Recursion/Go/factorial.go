97600abc074a2f63f6d791de36dcaf7a522ad09f
Peter Smith <peter@cruelandunusual.be>
2025-08-05 17:35:45 +0100

    added comments

--- Recursion/Go/factorial.go
+++ Recursion/Go/factorial.go
@@ -22,12 +22,18 @@ func main() {
 
 	for {
 		fmt.Print("Enter an integer to factorialise: > ")
+		// read until new line reached
 		input, err := reader.ReadString('\n')
 		if err != nil {
+			// log any read errors
 			log.Fatal(err)
 		}
+		// TrimSpace seems to work better than TrimSuffix(\n)
 		input = strings.TrimSpace(input)
+		// force the int conversion to unsigned in 64 to give a few more bits to play with in large numbers
+		// factorial 65 seems to be the limit
 		i, _ := strconv.ParseUint(input, 0, 64)
+
 		fmt.Printf("the factorial of %d is %d\n", i, factorial(i))
 	}
 }
